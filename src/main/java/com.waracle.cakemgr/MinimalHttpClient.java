6cfd502b494e288551346e70f82412fc4f93122e
Craig J Perry <craigp84@gmail.com>
2017-01-28 21:43:34 +0000

    fix(static analysis): Code hygene flagged by static analysis

--- src/main/java/com.waracle.cakemgr/MinimalHttpClient.java
+++ src/main/java/com.waracle.cakemgr/MinimalHttpClient.java
@@ -10,8 +10,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
 
-public class MinimalHttpClient {
-    public static InputStream httpGet(String url, String mimeType) throws IOException {
+class MinimalHttpClient {
+    static InputStream httpGet(String url, String mimeType) throws IOException {
         // In real life, there's no chance i'd be faffing about like this with just the standard library. Apache HttpClient FTW!
         HttpURLConnection c = (HttpURLConnection) new URL(url).openConnection();
         c.setRequestMethod("GET");
@@ -19,11 +19,11 @@ public class MinimalHttpClient {
         return c.getInputStream();
     }
 
-    public static List<CakeEntity> httpGetJson(String url) throws IOException {
+    static List<CakeEntity> httpGetJson(String url) throws IOException {
         return new ObjectMapper().readValue(httpGet(url, "application/json"), new TypeReference<List<CakeEntity>>(){});
     }
 
-    public static String httpGetHtml(String url) throws IOException {
+    static String httpGetHtml(String url) throws IOException {
         // NOTE: i couldn't remember the delimiter to use off the top of my head for this trick, had to google it to remind me because
         //       i never use this trick normally, i only know about it from a kata from years ago. Just trying to minimise deps
         //       i add to the project since you could solve this entire challenge using spring data rest and not write a single
