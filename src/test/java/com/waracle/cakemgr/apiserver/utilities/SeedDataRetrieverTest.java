8c7e6937301c51b47ae3ba4d5a7a542e3e234c36
WCL - Martin Warnett <coding.warnettconsultingltd@gmail.com>
2021-05-04 12:02:59 +0100

    Amended tests to check seed data correctly strips duplicates

--- src/test/java/com/waracle/cakemgr/apiserver/utilities/SeedDataRetrieverTest.java
+++ src/test/java/com/waracle/cakemgr/apiserver/utilities/SeedDataRetrieverTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SeedDataRetrieverTest {
     private SeedDataRetriever seedDataRetriever;
@@ -21,61 +21,32 @@ public class SeedDataRetrieverTest {
     public void checkThatSeedDataRetrieverReturnsCorrectSeedData() {
         var seedData = seedDataRetriever.getSeedData();
 
-        assertEquals(20, seedData.size());
+        assertEquals(5, seedData.size());
 
-        var expectedCakes = generateExpectedSeedDataCakeTypes();
-        assertEquals(seedData.get(0), expectedCakes.get(SeedDataCakeEnum.LEMON_CHEESECAKE));
-        assertEquals(seedData.get(1), expectedCakes.get(SeedDataCakeEnum.VICTORIA_SPONGE));
-        assertEquals(seedData.get(2), expectedCakes.get(SeedDataCakeEnum.CARROT_CAKE));
-        assertEquals(seedData.get(3), expectedCakes.get(SeedDataCakeEnum.BANANA_CAKE));
-        assertEquals(seedData.get(4), expectedCakes.get(SeedDataCakeEnum.BIRTHDAY_CAKE));
-        assertEquals(seedData.get(5), expectedCakes.get(SeedDataCakeEnum.LEMON_CHEESECAKE));
-        assertEquals(seedData.get(6), expectedCakes.get(SeedDataCakeEnum.VICTORIA_SPONGE));
-        assertEquals(seedData.get(7), expectedCakes.get(SeedDataCakeEnum.CARROT_CAKE));
-        assertEquals(seedData.get(8), expectedCakes.get(SeedDataCakeEnum.BANANA_CAKE));
-        assertEquals(seedData.get(9), expectedCakes.get(SeedDataCakeEnum.BIRTHDAY_CAKE));
-        assertEquals(seedData.get(10), expectedCakes.get(SeedDataCakeEnum.LEMON_CHEESECAKE));
-        assertEquals(seedData.get(11), expectedCakes.get(SeedDataCakeEnum.VICTORIA_SPONGE));
-        assertEquals(seedData.get(12), expectedCakes.get(SeedDataCakeEnum.CARROT_CAKE));
-        assertEquals(seedData.get(13), expectedCakes.get(SeedDataCakeEnum.BANANA_CAKE));
-        assertEquals(seedData.get(14), expectedCakes.get(SeedDataCakeEnum.BIRTHDAY_CAKE));
-        assertEquals(seedData.get(15), expectedCakes.get(SeedDataCakeEnum.LEMON_CHEESECAKE));
-        assertEquals(seedData.get(16), expectedCakes.get(SeedDataCakeEnum.VICTORIA_SPONGE));
-        assertEquals(seedData.get(17), expectedCakes.get(SeedDataCakeEnum.CARROT_CAKE));
-        assertEquals(seedData.get(18), expectedCakes.get(SeedDataCakeEnum.BANANA_CAKE));
-        assertEquals(seedData.get(19), expectedCakes.get(SeedDataCakeEnum.BIRTHDAY_CAKE));
-    }
-
-    private enum SeedDataCakeEnum {
-        LEMON_CHEESECAKE,
-        VICTORIA_SPONGE,
-        CARROT_CAKE,
-        BANANA_CAKE,
-        BIRTHDAY_CAKE;
-    }
-
-    private Map<SeedDataCakeEnum, SeedDataCake> generateExpectedSeedDataCakeTypes() {
-        var seedDataCakeTypes = new HashMap<SeedDataCakeEnum, SeedDataCake>();
-        seedDataCakeTypes.put(SeedDataCakeEnum.BANANA_CAKE,
+        assertTrue(seedData.contains(
                 new SeedDataCake("Banana cake",
                         "Donkey kongs favourite",
-                        "http://ukcdn.ar-cdn.com/recipes/xlarge/ff22df7f-dbcd-4a09-81f7-9c1d8395d936.jpg"));
-        seedDataCakeTypes.put(SeedDataCakeEnum.BIRTHDAY_CAKE,
+                        "http://ukcdn.ar-cdn.com/recipes/xlarge/ff22df7f-dbcd-4a09-81f7-9c1d8395d936.jpg")
+        ));
+        assertTrue(seedData.contains(
                 new SeedDataCake("Birthday cake",
                         "a yearly treat",
-                        "http://cornandco.com/wp-content/uploads/2014/05/birthday-cake-popcorn.jpg"));
-        seedDataCakeTypes.put(SeedDataCakeEnum.CARROT_CAKE,
+                        "http://cornandco.com/wp-content/uploads/2014/05/birthday-cake-popcorn.jpg")
+        ));
+        assertTrue(seedData.contains(
                 new SeedDataCake("Carrot cake",
                         "Bugs bunnys favourite",
-                        "http://www.villageinn.com/i/pies/profile/carrotcake_main1.jpg"));
-        seedDataCakeTypes.put(SeedDataCakeEnum.LEMON_CHEESECAKE,
+                        "http://www.villageinn.com/i/pies/profile/carrotcake_main1.jpg")
+        ));
+        assertTrue(seedData.contains(
                 new SeedDataCake("Lemon cheesecake",
                         "A cheesecake made of lemon",
-                        "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"));
-        seedDataCakeTypes.put(SeedDataCakeEnum.VICTORIA_SPONGE,
+                        "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg")
+        ));
+        assertTrue(seedData.contains(
                 new SeedDataCake("victoria sponge",
                         "sponge with jam",
-                        "http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg"));
-        return seedDataCakeTypes;
+                        "http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg")
+        ));
     }
 }
