2a8d2f1522a5d006ef0d8472d4649b4850830681
WCL - Martin Warnett <coding.warnettconsultingltd@gmail.com>
2021-05-04 10:35:39 +0100

    reverted previous change to Cake; created new record entity to model the seed data instead

--- src/main/java/com/waracle/cakemgr/entities/SeedDataCake.java
+++ src/main/java/com/waracle/cakemgr/entities/SeedDataCake.java
@@ -1,15 +1,13 @@
 package com.waracle.cakemgr.entities;
 
 import java.net.URI;
-import java.util.UUID;
 
 /**
- * Models the Cake entity, containing the following attributes :-
+ * Models the Cake seed data entities, containing the following attributes :-
  *
- * - id - the UUID of the Cake.
  * - title - the title pf the Cake.
  * - description - the description of the Cake.
  * - imageURL - the URI of the Cake image.
  */
-public record Cake(UUID id, String title, String description, URI imageURL) {
+public record SeedDataCake(String title, String description, URI imageURL) {
 }
