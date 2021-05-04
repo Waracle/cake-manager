fa32807b9b9c6355b20f09e0a6370021b944caa6
WCL - Martin Warnett <coding.warnettconsultingltd@gmail.com>
2021-05-04 10:31:26 +0100

    Removed id field for now - jumped ahead as seed data has no obvious id

--- src/main/java/com/waracle/cakemgr/entities/Cake.java
+++ src/main/java/com/waracle/cakemgr/entities/Cake.java
@@ -6,10 +6,9 @@ import java.util.UUID;
 /**
  * Models the Cake entity, containing the following attributes :-
  *
- * - id - the UUID of the Cake
  * - title - the title pf the Cake.
  * - description - the description of the Cake.
  * - imageURL - the URI of the Cake image.
  */
-public record Cake(UUID id, String title, String description, URI imageURL) {
+public record Cake(String title, String description, URI imageURL) {
 }
