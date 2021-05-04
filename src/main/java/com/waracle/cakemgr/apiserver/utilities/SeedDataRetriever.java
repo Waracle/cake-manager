8576298228aa3a01b682f01b35c9f3f8bac1a64e
WCL - Martin Warnett <coding.warnettconsultingltd@gmail.com>
2021-05-04 12:04:53 +0100

    Amended SeedDataRetriever to strip duplications

--- src/main/java/com/waracle/cakemgr/apiserver/utilities/SeedDataRetriever.java
+++ src/main/java/com/waracle/cakemgr/apiserver/utilities/SeedDataRetriever.java
@@ -5,18 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waracle.cakemgr.entities.SeedDataCake;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SeedDataRetriever {
-    public List<SeedDataCake> getSeedData() {
-        final var seedDataCakes = new ArrayList<SeedDataCake>();
+    public Set<SeedDataCake> getSeedData() {
+        final var seedDataCakes = new HashSet<SeedDataCake>();
 
         try {
             var objectMapper = new ObjectMapper();
             seedDataCakes.addAll(objectMapper.readValue(new URL("https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json"),
-                    new TypeReference<List<SeedDataCake>>() {}));
+                    new TypeReference<Set<SeedDataCake>>() {}));
 
         } catch (Exception e) {
             e.printStackTrace();
